e46e7327e57a4a948b5ea6577d0141f920b66ca6
Asnassta <79850172+Asnassta@users.noreply.github.com>
2021-09-09 17:48:11 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,23 @@ $(document).ready(function() {
 	});
 	/*============/search-toggle================*/
 
+	/*===============Form-popup=================*/
+	$(".btn-open").on("click", function(event) {
+		event.preventDefault();
+		$(".form-popup").fadeIn(333);
+ 
+		$('.form-popup').css('top', $(window).scrollTop()+'px');
+		$(".form-popup__inner").fadeIn(333);
+		$('body').addClass("hidden");
+	});
+	$(".form-popup__close,  .closex, btn-close").on("click", function(event) {
+		event.preventDefault();
+		$(".form-popup").fadeOut('222');
+		$(".form-popup__inner").fadeOut(222);
+		$('body').removeClass("hidden");
+	});
+/*==============/Form-popup=================*/
+
 
 	/*======Slider-intro=============*/
 	$('.intro__slider').slick({
